cea0ec3040a40cb472e5993aafd86978b7a5f750
Gabriel Duarte <duartearias2002@gmail.com>
2024-07-24 11:07:53 -0500

    colocar validators estados seguimientos
    Se colocan validators en la identidad de estados seguimiento, y se crean rutas y controladores faltantes

--- src/controllers/estados-seguimientos_controller.ts
+++ src/controllers/estados-seguimientos_controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import { EstadosSeguimiento } from "../entities/estados-seguimiento";
+import { validate, Validator } from "class-validator";
 
 export async function getEstadosSeguimientos(req: Request, res: Response){
     try {
@@ -11,5 +12,185 @@ export async function getEstadosSeguimientos(req: Request, res: Response){
         if (error instanceof Error) {
             return res.status(500).json({ message: error.message });
         }
+    }
+}
+
+export async function getEstadosSeguimiento(req: Request, res: Response){
+    try {
+        
+        const { id } = req.params;
+
+        if (!id) {
+            return res.status(400).json({message: "Id is required"});
+        }
+
+        const estadoSeguimientoId = parseInt(id);
+
+        if (isNaN(estadoSeguimientoId)) {
+            return res.status(400).json({message: "Id must be a number"});
+        }
+
+        const estadoSeguimiento = await EstadosSeguimiento.findOneBy({id: parseInt(id)});
+
+        if (!estadoSeguimiento) {
+            return res.status(404).json({message: "Estado de seguimiento not found"});
+        }
+
+        return res.json(estadoSeguimiento);
+
+    } catch (error) {
+        
+        console.error(error);
+
+        if (error instanceof Error) {
+            return res.status(500).json({message: error.message});
+        }
+
+        return res.status(500).json({message: "Internal Server Error"});
+
+    }
+}
+
+export async function createEstadosSeguimiento(req: Request, res: Response){
+    try {
+        
+        const { name } = req.body;
+
+        if (!name) {
+            return res.status(400).json({message: "Name and status are required"});
+        }
+
+        const estadoSeguimientoExist = await EstadosSeguimiento.findOneBy({name});
+
+        if (estadoSeguimientoExist) {
+            return res.status(400).json({message: "Estado de seguimiento already exists"});
+        }
+
+        const estadoSeguimiento = new EstadosSeguimiento();
+        estadoSeguimiento.name = name;
+        estadoSeguimiento.status = true;
+
+        const errors = await validate(estadoSeguimiento);
+
+        if (errors.length > 0) {
+
+            const errorsMessage = errors.map(err => ({
+                property: err.property,
+                constraints: err.constraints
+            }));
+
+            return res.status(400).json({message: "Error creating estado de seguimiento", errors: errorsMessage});
+
+        }
+
+        await estadoSeguimiento.save();
+
+        return res.json(estadoSeguimiento);
+
+    } catch (error) {
+        
+        console.error(error);
+
+        if (error instanceof Error) {
+            return res.status(500).json({message: error.message});
+        }
+
+        return res.status(500).json({message: "Internal Server Error"});
+
+    }
+}
+
+export async function updateEstadosSeguimiento(req: Request, res: Response){
+    try {
+        
+        const { id } = req.params;
+        
+        if (!id) {
+            return res.status(400).json({message: "Id is required"});
+        }
+        
+        const { name, status } = req.body;
+        
+        if (!name || !status) {
+
+            return res.status(400).json({message: "Name and status are required"});
+        }
+
+        const estadoSeguimientoId = parseInt(id);
+
+        if (isNaN(estadoSeguimientoId)) {
+            return res.status(400).json({message: "Id must be a number"});
+        }
+
+        const estadoSeguimiento = await EstadosSeguimiento.findOneBy({id: parseInt(id)});
+        if (!estadoSeguimiento) {
+            return res.status(404).json({message: "Estado de seguimiento not found"});
+        }
+
+        estadoSeguimiento.name = name;
+        estadoSeguimiento.status = status;
+
+        const errors = await validate(estadoSeguimiento);
+        if (errors.length > 0) {
+            const errorsMessage = errors.map(err => ({
+                property: err.property,
+                constraints: err.constraints
+            }));
+
+            return res.status(400).json({message: "Error updating estado de seguimiento", errors: errorsMessage});
+        }
+
+        await estadoSeguimiento.save();
+
+        return res.json(estadoSeguimiento);
+
+    } catch (error) {
+        
+        console.error(error);
+
+        if (error instanceof Error) {
+            return res.status(500).json({message: error.message});
+        }
+
+        return res.status(500).json({message: "Internal Server Error"});
+
+    }
+}
+
+export async function deleteEstadosSeguimiento(req: Request, res: Response){
+    try {
+        
+        const { id } = req.params;
+
+        if (!id) {
+            return res.status(400).json({message: "Id is required"});
+        }
+
+        const estadoSeguimientoId = parseInt(id);
+
+        if (isNaN(estadoSeguimientoId)) {
+            return res.status(400).json({message: "Id must be a number"});
+        }
+
+        const estadoSeguimiento = await EstadosSeguimiento.findOneBy({id: parseInt(id)});
+
+        if (!estadoSeguimiento) {
+            return res.status(404).json({message: "Estado de seguimiento not found"});
+        }
+
+        await estadoSeguimiento.remove();
+
+        return res.json({message: "Estado de seguimiento deleted"});
+
+    } catch (error) {
+        
+        console.error(error);
+
+        if (error instanceof Error) {
+            return res.status(500).json({message: error.message});
+        }
+
+        return res.status(500).json({message: "Internal Server Error"});
+
     }
 }
--- src/routes/estados-seguimientos_routes.ts
+++ src/routes/estados-seguimientos_routes.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { getEstadosSeguimientos } from "../controllers/estados-seguimientos_controller";
+import { createEstadosSeguimiento, deleteEstadosSeguimiento, getEstadosSeguimiento, getEstadosSeguimientos, updateEstadosSeguimiento } from "../controllers/estados-seguimientos_controller";
 
 const router = Router();
 
 router.get("/estados-seguimientos", getEstadosSeguimientos);
+
+router.get("/estados-seguimientos/:id", getEstadosSeguimiento);
+
+router.post("/estados-seguimientos", createEstadosSeguimiento);
+
+router.put("/estados-seguimientos/:id", updateEstadosSeguimiento);
+
+router.delete("/estados-seguimientos/:id", deleteEstadosSeguimiento);
 
 export default router;
