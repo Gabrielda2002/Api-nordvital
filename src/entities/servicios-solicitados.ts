b61136977c0c010b2c22ea0b20664c76a3df0206
Gabriel Duarte <duartearias2002@gmail.com>
2024-07-30 08:13:14 -0500

    colocando validators entidad servicio solicitado
    Se colocan validators en la entidad de servicio solicitado, y se crean controladores y rutas faltantes

--- src/entities/servicios-solicitados.ts
+++ src/entities/servicios-solicitados.ts
@@ -1,3 +1,4 @@
+import { IsBoolean, IsNotEmpty, IsString, Length } from "class-validator";
 import { BaseEntity, Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 @Entity({name: "serviciosolicitado"})
@@ -7,13 +8,21 @@ export class ServiciosSolicitados extends BaseEntity {
     id: number;
 
     @Column({name: "Codigo"})
+    @IsString()
+    @IsNotEmpty({message: "El código es requerido"})
+    @Length(1, 10, {message: "El código debe tener entre $constraint1 y $constraint2 caracteres"})
     code: string;
 
     @Column({name: "NombreSolicitado"})
+    @IsString()
+    @IsNotEmpty({message: "El nombre es requerido"})
+    @Length(1, 100, {message: "El nombre debe tener entre $constraint1 y $constraint2 caracteres"})
     name: string;
 
     @Column({name: "Estado"}) 
-    status: string;
+    @IsBoolean()
+    @IsNotEmpty({message: "El estado es requerido"})
+    status: boolean;
 
     @UpdateDateColumn({ name: "fecha-actualizacion" })
     updatedAt: Date
