263a24f9b19e13fac16293a5e5867f3a412f45ba
Gabriel Duarte <duartearias2002@gmail.com>
2024-07-30 15:21:50 -0500

    colocando validators entidad usuarios
    Se colocan validators en la entidad de usurio, y se crean controladores y rutas faltantes

--- src/controllers/usuario_controller.ts
+++ src/controllers/usuario_controller.ts
@@ -1,17 +1,171 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { Usuarios } from "../entities/usuarios";
+import { validate } from "class-validator";
+import bcrypt from "bcrypt";
 
-export async function getAllUsuarios(req: Request, res: Response){
-    try {
-        
-        const usuarios = await Usuarios.find();
-        return res.json(usuarios);
+export async function getAllUsuarios(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const usuarios = await Usuarios.find();
+    return res.json(usuarios);
+  } catch (error) {
+    next(error);
+  }
+}
 
-    } catch (error) {
-        if (error instanceof Error) {
-            console.log(error.message);
-            res.status(500).json({message: "Internal Server Error"});
-            
-        }
+export async function getUsuario(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const { id } = req.params;
+
+    const usuario = await Usuarios.findOneBy({ id: parseInt(id) });
+
+    if (!usuario) {
+      return res.status(404).json({ message: "Usuario no encontrado" });
     }
+
+    return res.json(usuario);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function createUsuario(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const {
+      dniNumber,
+      name,
+      lastName,
+      dniType,
+      email,
+      password,
+      date,
+      municipio,
+      rol,
+    } = req.body;
+
+    const usuario = new Usuarios();
+    usuario.dniNumber = dniNumber;
+    usuario.name = name;
+    usuario.lastName = lastName;
+    usuario.dniType = dniType;
+    usuario.email = email;
+
+    const saltRounds = 10;
+    usuario.password = await bcrypt.hash(password, saltRounds); 
+    usuario.date = date;
+    usuario.status = true;
+    usuario.municipio = municipio;
+    usuario.rol = rol;
+
+    const errors = await validate(usuario);
+
+    if (errors.length > 0) {
+        const messageError = errors.map((error) => ({
+            property: error.property,
+            constraints: error.constraints
+        }));
+
+        return res.status(400).json({"message" : "Ocurrio un error: ", messageError});
+    }
+
+    await usuario.save();
+
+    return res.json(usuario);
+
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function updateUsuario(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const { id } = req.params;
+    const {
+      dniNumber,
+      name,
+      lastName,
+      dniType,
+      email,
+      password,
+      date,
+      status,
+      municipio,
+      rol,
+    } = req.body;
+
+    const usuario = await Usuarios.findOneBy({ id: parseInt(id) });
+
+    if (!usuario) {
+      return res.status(404).json({ message: "Usuario no encontrado" });
+    }
+
+    usuario.dniNumber = dniNumber;
+    usuario.name = name;
+    usuario.lastName = lastName;
+    usuario.dniType = dniType;
+    usuario.email = email;
+
+    if (password) {
+        const saltRounds = 10;
+        usuario.password = await bcrypt.hash(password, saltRounds);
+    }
+    usuario.date = date;
+    usuario.status = status;
+    usuario.municipio = municipio;
+    usuario.rol = rol;
+
+    const errors = await validate(usuario);
+
+    if (errors.length > 0) {
+        const messageError = errors.map((error) => ({
+            property: error.property,
+            constraints: error.constraints
+        }));
+
+        return res.status(400).json({"message" : "Ocurrio un error: ", messageError});
+    }
+
+    await usuario.save();
+
+    return res.json(usuario);
+  } catch (error) {
+    next(error);
+  }
+}
+
+export async function deleteUsuario(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const { id } = req.params;
+
+    const usuario = await Usuarios.findOneBy({ id: parseInt(id) });
+
+    if (!usuario) {
+      return res.status(404).json({ message: "Usuario no encontrado" });
+    }
+
+    await usuario.remove();
+
+    return res.json({ message: "Usuario eliminado" });
+  } catch (error) {
+    next(error);
+  }
 }
--- src/routes/usuarios_routes.ts
+++ src/routes/usuarios_routes.ts
@@ -1,8 +1,17 @@
 import { Router } from "express";
-import { getAllUsuarios } from "../controllers/usuario_controller";
+import { createUsuario, deleteUsuario, getAllUsuarios, getUsuario, updateUsuario } from "../controllers/usuario_controller";
+import { validarId } from "../middlewares/validar-id";
 
 const router = Router();
 
 router.get('/usuarios', getAllUsuarios);
+
+router.get('/usuarios/:id', validarId, getUsuario);
+
+router.post('/usuarios', createUsuario);
+
+router.put('/usuarios/:id', validarId, updateUsuario);
+
+router.delete('/usuarios/:id', validarId, deleteUsuario);
 
 export default router;
