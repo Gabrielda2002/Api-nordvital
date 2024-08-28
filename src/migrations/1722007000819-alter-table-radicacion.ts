6af88ae44b5ad1ad4bded3e62ac51b2f1c235526
Gabriel Duarte <duartearias2002@gmail.com>
2024-08-28 11:10:17 -0500

    Se modifica migracion
    se modifica la migracion 1722007000819-alter-table-radicacion, para evitar errores de nombres de las llaves foraneas para su eliminacion de la tabla radicacion

--- src/migrations/1722007000819-alter-table-radicacion.ts
+++ src/migrations/1722007000819-alter-table-radicacion.ts
@@ -18,43 +18,62 @@ import {
   ];
   
   // Nombres reales de las claves foráneas en la base de datos
-  const foreignKeys = [
-    { columnName: "TipoDocumento", foreignKeyName: "radicacion_ibfk_13" },
-    { columnName: "Convenio", foreignKeyName: "radicacion_ibfk_2" },
-    { columnName: "IpsPrimaria", foreignKeyName: "radicacion_ibfk_3" },
-  ];
+
   
   export class AlterTableRadicacion1722007000819 implements MigrationInterface {
     name = "AlterTableRadicacion1722007000819";
   
     public async up(queryRunner: QueryRunner): Promise<void> {
+
       // Obtener la tabla 'radicacion'
-      const table = await queryRunner.getTable("radicacion");
-  
-      if (!table) {
-        console.error("La tabla 'radicacion' no se encontró.");
-        return;
+
+      const columnExists = await queryRunner.hasColumn("radicacion", "TipoDocumento");
+      if (columnExists) {
+          // * Verificar si la llave foránea existe en la tabla "soportes"
+          const table = await queryRunner.getTable("radicacion");
+          const foreignKey = table?.foreignKeys.find(fk => fk.columnNames.indexOf("TipoDocumento") !== -1);
+
+          if (foreignKey) {
+              // * Si la llave foránea existe, eliminarla
+              await queryRunner.dropForeignKey("radicacion", foreignKey);
+          }
+
+          // * Si la columna existe, eliminarla
+          await queryRunner.dropColumn("radicacion", "TipoDocumento");
       }
-  
-      console.log("Claves foráneas existentes en la tabla:");
-      table.foreignKeys.forEach((fk) => {
-        console.log(`Nombre: ${fk.name}, Columnas: ${fk.columnNames}`);
-      });
-  
-      // Eliminar claves foráneas
-      for (const fk of foreignKeys) {
-        const foreignKey = table.foreignKeys.find(
-          (foreignKey) => foreignKey.name === fk.foreignKeyName
-        );
-  
-        console.log(`Buscando clave foránea: ${fk.foreignKeyName}`);
-  
-        if (foreignKey) {
-          console.log(`Eliminando clave foránea: ${fk.foreignKeyName}`);
-          await queryRunner.dropForeignKey("radicacion", foreignKey);
-        } else {
-          console.warn(`No se encontró la clave foránea: ${fk.foreignKeyName}`);
-        }
+
+      // * Eliminar las columnas y las llaves foránea convenio"
+
+      const columnConvenioExists = await queryRunner.hasColumn("radicacion", "Convenio");
+      if (columnConvenioExists) {
+          // * Verificar si la llave foránea existe en la tabla "soportes"
+          const table = await queryRunner.getTable("radicacion");
+          const foreignKey = table?.foreignKeys.find(fk => fk.columnNames.indexOf("Convenio") !== -1);
+
+          if (foreignKey) {
+              // * Si la llave foránea existe, eliminarla
+              await queryRunner.dropForeignKey("radicacion", foreignKey);
+          }
+
+          // * Si la columna existe, eliminarla
+          await queryRunner.dropColumn("radicacion", "Convenio");
+      }
+
+      // * Eliminar las columnas y las llaves foránea IpsPrimaria"
+
+      const columnIpsPrimariaExists = await queryRunner.hasColumn("radicacion", "IpsPrimaria");
+      if (columnIpsPrimariaExists) {
+          // * Verificar si la llave foránea existe en la tabla "soportes"
+          const table = await queryRunner.getTable("radicacion");
+          const foreignKey = table?.foreignKeys.find(fk => fk.columnNames.indexOf("IpsPrimaria") !== -1);
+
+          if (foreignKey) {
+              // * Si la llave foránea existe, eliminarla
+              await queryRunner.dropForeignKey("radicacion", foreignKey);
+          }
+
+          // * Si la columna existe, eliminarla
+          await queryRunner.dropColumn("radicacion", "IpsPrimaria");
       }
   
       // Eliminar columnas
@@ -65,73 +84,109 @@ import {
     }
   
     public async down(queryRunner: QueryRunner): Promise<void> {
-      // Agregar nuevamente las columnas eliminadas
-      await queryRunner.addColumns("radicacion", [
-        new TableColumn({ name: "TipoDocumento", type: "int", isNullable: true }),
-        new TableColumn({
-          name: "Identificacion",
-          type: "varchar",
-          length: "255",
-          isNullable: true,
-        }),
-        new TableColumn({
-          name: "NombreCompleto",
-          type: "varchar",
-          length: "255",
-          isNullable: true,
-        }),
-        new TableColumn({
-          name: "NumeroCel",
-          type: "varchar",
-          length: "20",
-          isNullable: true,
-        }),
-        new TableColumn({
-          name: "TelFijo",
-          type: "varchar",
-          length: "20",
-          isNullable: true,
-        }),
-        new TableColumn({
-          name: "Email",
-          type: "varchar",
-          length: "255",
-          isNullable: true,
-        }),
-        new TableColumn({
-          name: "Direccion",
-          type: "varchar",
-          length: "255",
-          isNullable: true,
-        }),
-        new TableColumn({ name: "Convenio", type: "int", isNullable: true }),
-        new TableColumn({ name: "IpsPrimaria", type: "int", isNullable: true }),
-      ]);
-  
-      // Volver a agregar claves foráneas
-      await queryRunner.createForeignKeys("radicacion", [
-        new TableForeignKey({
-          name: "FK_TipoDocumento",
-          columnNames: ["TipoDocumento"],
-          referencedColumnNames: ["id"],
-          referencedTableName: "tipo_documento",
-          onDelete: "SET NULL",
-        }),
-        new TableForeignKey({
-          name: "FK_Convenio",
-          columnNames: ["Convenio"],
-          referencedColumnNames: ["id"],
-          referencedTableName: "convenio",
-          onDelete: "SET NULL",
-        }),
-        new TableForeignKey({
-          name: "FK_IpsPrimaria",
-          columnNames: ["IpsPrimaria"],
-          referencedColumnNames: ["id"],
-          referencedTableName: "ips_primaria",
-          onDelete: "SET NULL",
-        }),
-      ]);
+      // Restaurar las columnas
+    await queryRunner.addColumn(
+      "radicacion",
+      new TableColumn({
+        name: "TipoDocumento",
+        type: "int",
+      })
+    );
+
+    await queryRunner.addColumn(
+      "radicacion",
+      new TableColumn({
+        name: "Identificacion",
+        type: "varchar", 
+      })
+    );
+
+    await queryRunner.addColumn(
+      "radicacion",
+      new TableColumn({
+        name: "NombreCompleto",
+        type: "varchar", 
+      })
+    );
+
+    await queryRunner.addColumn(
+      "radicacion",
+      new TableColumn({
+        name: "NumeroCel",
+        type: "varchar",
+      })
+    );
+
+    await queryRunner.addColumn(
+      "radicacion",
+      new TableColumn({
+        name: "TelFijo",
+        type: "varchar", 
+      })
+    );
+
+    await queryRunner.addColumn(
+      "radicacion",
+      new TableColumn({
+        name: "Email",
+        type: "varchar",
+      })
+    );
+
+    await queryRunner.addColumn(
+      "radicacion",
+      new TableColumn({
+        name: "Direccion",
+        type: "varchar", 
+      })
+    );
+
+    await queryRunner.addColumn(
+      "radicacion",
+      new TableColumn({
+        name: "Convenio",
+        type: "int", 
+      })
+    );
+
+    await queryRunner.addColumn(
+      "radicacion",
+      new TableColumn({
+        name: "IpsPrimaria",
+        type: "int", // Reemplazar con el tipo de dato correcto
+      })
+    );
+
+    // Restaurar claves foráneas
+    await queryRunner.createForeignKey(
+      "radicacion",
+      new TableForeignKey({
+        columnNames: ["TipoDocumento"],
+        referencedColumnNames: ["IdDocumento"], // Reemplazar con el nombre correcto de la columna referenciada
+        referencedTableName: "documento", // Reemplazar con el nombre correcto de la tabla referenciada
+        onDelete: "SET NULL", // Ajustar según sea necesario
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      "radicacion",
+      new TableForeignKey({
+        columnNames: ["Convenio"],
+        referencedColumnNames: ["IdConvenio"], // Reemplazar con el nombre correcto de la columna referenciada
+        referencedTableName: "convenio", // Reemplazar con el nombre correcto de la tabla referenciada
+        onDelete: "SET NULL", // Ajustar según sea necesario
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      "radicacion",
+      new TableForeignKey({
+        columnNames: ["IpsPrimaria"],
+        referencedColumnNames: ["IdIpsPrimaria"], // Reemplazar con el nombre correcto de la columna referenciada
+        referencedTableName: "ipsprimaria", // Reemplazar con el nombre correcto de la tabla referenciada
+        onDelete: "SET NULL", // Ajustar según sea necesario
+      })
+    );
     }
   }
   
