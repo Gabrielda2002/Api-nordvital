4b8bc4b3385a57350ce7a504f8241d51dd985163
Gabriel Duarte <duartearias2002@gmail.com>
2024-08-29 14:59:09 -0500

    controlador reporte filtro y ruta

--- src/routes/report-excel_routes.ts
+++ src/routes/report-excel_routes.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { authenticate } from "../middlewares/auth";
 import { authorizeRoles } from "../middlewares/authorize-roles";
-import { downloadReportExcel } from "../controllers/report-excel_controller";
+import { downloadReportExcel, downloadReportExcelFilter } from "../controllers/report-excel_controller";
 
 const router = Router();
 
 router.get('/report-excel', authenticate, authorizeRoles(['1', '2']) , downloadReportExcel)
+
+router.post('/report-excel-filtro', authenticate, authorizeRoles(['1', '2']) , downloadReportExcelFilter)
 
 export default router;
