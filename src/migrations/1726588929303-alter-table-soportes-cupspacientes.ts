e626a52c7ac64aa19501eb53b2e0108f141d9590
Gabriel Duarte <duartearias2002@gmail.com>
2024-09-20 10:33:57 -0500

    correccion migracion para la tabla cupspacientes

--- src/migrations/1726588929303-alter-table-soportes-cupspacientes.ts
+++ src/migrations/1726588929303-alter-table-soportes-cupspacientes.ts
@@ -3,64 +3,28 @@ import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
 export class AlterTableSoportesCupspacientes1726588929303 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-         // Add name_saved column to soportes table
-        await queryRunner.addColumn("soportes", new TableColumn({
-            name: "name_saved",
-            type: "varchar",
-            isNullable: true
-        }));
+        // Eliminar las columnas FechaRegistro y UltimaModificacion de la tabla cupspaciente
+        await queryRunner.dropColumn("cupspaciente", "FechaRegistro");
+        await queryRunner.dropColumn("cupspaciente", "UltimaModificacion");
 
-        await queryRunner.query(`
-            UPDATE \`cupspaciente\`
-            SET \`UltimaModificacion\` = STR_TO_DATE(\`UltimaModificacion\`, '%Y-%m-%d %h:%i %p')
-        `)
-
-        await queryRunner.query(`
-            UPDATE \`cupspaciente\`
-            SET \`FechaRegistro\` = STR_TO_DATE(\`FechaRegistro\`, '%Y-%m-%d %h:%i %p')
-        `)
-
-         // Change data type of UltimaModificacion and FechaRegistro to timestamp in cupspacientes table
-        await queryRunner.changeColumn("cupspaciente", "UltimaModificacion", new TableColumn({
-            name: "UltimaModificacion",
-            type: "timestamp",
-            isNullable: false
-        }));
-
-        await queryRunner.changeColumn("cupspaciente", "FechaRegistro", new TableColumn({
+        // Crear las columnas FechaRegistro y UltimaModificacion en la tabla cupspaciente
+        await queryRunner.addColumn("cupspaciente", new TableColumn({
             name: "FechaRegistro",
             type: "timestamp",
+            default: "CURRENT_TIMESTAMP",
             isNullable: false
-
         }));
-
-        
-        // * en caso de que ocurra un error a la hora de correr la migracion tal y como esta
-        // crear la columna FechaRegistro en la tabla cupspaciente
-        // await queryRunner.addColumn("cupspaciente", new TableColumn({
-        //     name: "FechaRegistro",
-        //     type: "timestamp",
-        //     default: "CURRENT_TIMESTAMP",
-        //     isNullable: false
-        // }));
+        await queryRunner.addColumn("cupspaciente", new TableColumn({
+            name: "UltimaModificacion",
+            type: "timestamp",
+            default: "CURRENT_TIMESTAMP",
+            isNullable: false
+        }));
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-
-        // Remove name_saved column from soportes table
-        await queryRunner.dropColumn("soportes", "name_saved");
-
-        // Change data type of UltimaModificacion and FechaRegistro back to original type in cupspacientes table
-        await queryRunner.changeColumn("cupspaciente", "UltimaModificacion", new TableColumn({
-            name: "UltimaModificacion",
-            type: "varchar", 
-            isNullable: false
-        }));
-
-        await queryRunner.changeColumn("cupspaciente", "FechaRegistro", new TableColumn({
-            name: "FechaRegistro",
-            type: "varchar", 
-            isNullable: false
-        }));
+        // Eliminar las columnas FechaRegistro y UltimaModificacion de la tabla cupspaciente
+        await queryRunner.dropColumn("cupspaciente", "FechaRegistro");
+        await queryRunner.dropColumn("cupspaciente", "UltimaModificacion");
     }
 }
