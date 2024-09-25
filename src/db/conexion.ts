a13940a9aaff686abad5e6b04ada48b056ce20aa
Gabriel Duarte <duartearias2002@gmail.com>
2024-09-25 11:04:06 -0500

    ajuste para servidor
    Se hacen ajustes en conexion para el servidor y se crea .env de ejemplo

--- src/db/conexion.ts
+++ src/db/conexion.ts
@@ -3,9 +3,9 @@ import { DataSource } from "typeorm";
 export const AppDataSource = new DataSource({
   type: "mysql",
   host: process.env.DB_HOST || "localhost",
-  username: process.env.DB_USER || "nordvita_Developers",
-password: process.env.DB_PASS || "NaRDViToL1.0.5.58",
-  database: process.env.DB_NAME || "nordvita_nordvitalips-api-test",
+  username: process.env.DB_USER || "nordvitalDb",
+password: process.env.DB_PASS || "nordvital-ips-db",
+  database: process.env.DB_NAME || "nordvitalips",
   charset: "utf8mb4",
   synchronize: false,
   port: parseInt(process.env.DB_PORT || "3306"),
