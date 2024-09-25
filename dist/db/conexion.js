3eaba6032e493428988bbd944714c46b32309008
Gabriel Duarte <duartearias2002@gmail.com>
2024-09-25 11:33:41 -0500

    ajuste buildeo

--- dist/db/conexion.js
+++ dist/db/conexion.js
@@ -4,9 +4,9 @@ exports.AppDataSource = void 0;
 const typeorm_1 = require("typeorm");
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "mysql",
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DB_HOST || "44.222.149.53",
     username: process.env.DB_USER || "nordvitalDb",
-    password: process.env.DB_PASS || "nordvital-ips-db",
+    password: process.env.DB_PASS || "nordvital-api-db",
     database: process.env.DB_NAME || "nordvitalips",
     charset: "utf8mb4",
     synchronize: false,
