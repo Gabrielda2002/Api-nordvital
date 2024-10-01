709ca6435ba8e26d87d908aa53b4564545317017
Gabriel Duarte <duartearias2002@gmail.com>
2024-10-01 16:55:33 -0500

    Ajuste a migracion

--- src/migrations/1727816016045-alter-table-radicacion.ts
+++ src/migrations/1727816016045-alter-table-radicacion.ts
@@ -2,30 +2,31 @@ import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
 
 export class AlterTableRadicacion1727816016045 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropColumn("radicacion", "ServicioSolicitado");
-        await queryRunner.dropColumn("radicacion", "DescripcionCup");
-        await queryRunner.dropColumn("radicacion", "NombreSoporte");
-        await queryRunner.dropColumn("radicacion", "TipoSoporte");
-        await queryRunner.dropColumn("radicacion", "ObservacionAuditoria");
-        await queryRunner.dropColumn("radicacion", "Informacion");
+         await queryRunner.dropColumn("radicacion", "ServicioSolicitado");
+         await queryRunner.dropColumn("radicacion", "DescripcionCup");
+         await queryRunner.dropColumn("radicacion", "NombreSoporte");
+         await queryRunner.dropColumn("radicacion", "TipoSoporte");
+         await queryRunner.dropColumn("radicacion", "ObservacionAuditoria");
+         await queryRunner.dropColumn("radicacion", "Informacion");
+         await queryRunner.dropColumn("radicacion", "contenido");
 
-        // ? se busca la llave foranea de pacientes y si se encuentra se elimina
-    const unidadFuncionalExist = await queryRunner.hasColumn(
-        "radicacion",
-        "UnidadFuncional"
-      );
-      if (unidadFuncionalExist) {
-        const table = await queryRunner.getTable("radicacion");
-        const foreignKey = table?.foreignKeys.find(
-          (fk) => fk.columnNames.indexOf("UnidadFuncional") !== -1
-        );
+         //? se busca la llave foranea de pacientes y si se encuentra se elimina
+     const unidadFuncionalExist = await queryRunner.hasColumn(
+         "radicacion",
+         "UnidadFuncional"
+       );
+       if (unidadFuncionalExist) {
+         const table = await queryRunner.getTable("radicacion");
+         const foreignKey = table?.foreignKeys.find(
+           (fk) => fk.columnNames.indexOf("UnidadFuncional") !== -1
+         );
   
-        if (foreignKey) {
-          await queryRunner.dropForeignKey("radicacion", foreignKey);
-        }
+         if (foreignKey) {
+           await queryRunner.dropForeignKey("radicacion", foreignKey);
+         }
   
         await queryRunner.dropColumn("radicacion", "UnidadFuncional");
-      }
+       }
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
@@ -55,7 +56,7 @@ export class AlterTableRadicacion1727816016045 implements MigrationInterface {
             isNullable: true
         }));
         await queryRunner.addColumn("radicacion", new TableColumn({
-            name: "unidadFuncional",
+            name: "UnidadFuncional",
             type: "varchar",
             isNullable: true
         }));
