83735f79d4e4efa86dca672d16d9556a81ff6654
Gabriel Duarte <duartearias2002@gmail.com>
2024-10-01 16:27:57 -0500

    Ajuste conexion

--- dist/db/conexion.js
+++ dist/db/conexion.js
@@ -4,10 +4,10 @@ exports.AppDataSource = void 0;
 const typeorm_1 = require("typeorm");
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "mysql",
-    host: process.env.DB_HOST || "localhost",
-    username: process.env.DB_USERNAME || "root",
+    host: process.env.DB_HOST,
+    username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE || "nordvitalips-api-test",
+    database: process.env.DB_DATABASE,
     charset: "utf8mb4",
     synchronize: false,
     port: parseInt("3306"),
