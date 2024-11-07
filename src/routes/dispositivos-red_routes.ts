8bba732a7d060bd2ba676254b213b994781cacd9
Gabriel Duarte <duartearias2002@gmail.com>
2024-11-07 16:31:46 -0500

    buscar dispositivo red sede
    Se agrega controlador y ruta buscar dispositivo red por sede

--- src/routes/dispositivos-red_routes.ts
+++ src/routes/dispositivos-red_routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { authorizeRoles } from "../middlewares/authorize-roles";
 import { authenticate } from "../middlewares/auth";
-import { createDevice, deleteDevice, getAllDevices, getDevice, updateDevice } from "../controllers/dispositivos-red_controller";
+import { createDevice, deleteDevice, getAllDevices, getDevice, getDevicesBySede, updateDevice } from "../controllers/dispositivos-red_controller";
 import { validarId } from "../middlewares/validar-id";
 
 const router = Router();
@@ -15,5 +15,7 @@ router.post("/dispositivos-red", authenticate, authorizeRoles(['1']), createDevi
 router.put("/dispositivos-red/:id", authenticate, authorizeRoles(['1']),validarId, updateDevice);
 
 router.delete("/dispositivos-red/:id", authenticate, authorizeRoles(['1']),validarId, deleteDevice);
+
+router.get("/dispositivos-red-sede/:id", authenticate, authorizeRoles(['1']),validarId, getDevicesBySede);
 
 export default router;
