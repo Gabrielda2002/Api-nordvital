85f4a54d186509373fcf879f0e561a7c630e58b9
Gabriel Duarte <duartearias2002@gmail.com>
2024-11-13 11:40:30 -0500

    Migracion alter table seguimiento dispositivos red
    se crea relacion con usuarios  y se agregan los campos fechas de creacion y actualizacion de las tablas equipos y dispositivos red

---
+++ src/migrations/1731515304103-alter-table-seguimiento-dispositivos-red.ts
@@ -0,0 +1,53 @@
+import { MigrationInterface, QueryRunner, TableForeignKey, TableColumn } from "typeorm";
+
+export class AlterTableSeguimientoDispositivosRed1731515304103 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createForeignKey(
+            "seguimiento_dispositivos_red",
+            new TableForeignKey({
+                columnNames: ["dispositivo_id"],
+                referencedColumnNames: ["id"],
+                referencedTableName: "dispositivos_red",
+                onDelete: "CASCADE",
+            })
+        );
+
+        await queryRunner.addColumns("dispositivos_red", [
+            new TableColumn({
+                name: "fecha_creacion",
+                type: "timestamp",
+                default: "CURRENT_TIMESTAMP",
+            }),
+            new TableColumn({
+                name: "fecha_actualizacion",
+                type: "timestamp",
+                default: "CURRENT_TIMESTAMP",
+                onUpdate: "CURRENT_TIMESTAMP",
+            }),
+        ]);
+
+        await queryRunner.addColumns("equipos", [
+            new TableColumn({
+                name: "fecha_creacion",
+                type: "timestamp",
+                default: "CURRENT_TIMESTAMP",
+            }),
+            new TableColumn({
+                name: "fecha_actualizacion",
+                type: "timestamp",
+                default: "CURRENT_TIMESTAMP",
+                onUpdate: "CURRENT_TIMESTAMP",
+            }),
+        ]);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropForeignKey("seguimiento_dispositivos_red", "FK_dispositivo_red_id");
+
+        await queryRunner.dropColumn("dispositivos_red", "created_at");
+        await queryRunner.dropColumn("dispositivos_red", "updated_at");
+
+        await queryRunner.dropColumn("equipos", "created_at");
+        await queryRunner.dropColumn("equipos", "updated_at");
+    }
+}
