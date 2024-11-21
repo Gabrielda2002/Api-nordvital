5a91737fb71ae14aaf743961aac41fe764489535
Gabriel Duarte <duartearias2002@gmail.com>
2024-11-21 09:03:26 -0500

    ajuste swagger options

--- src/swagger-options.ts
+++ src/swagger-options.ts
@@ -1,6 +1,6 @@
 export const options = {
     definition: {
-        openaapi: "3.0.0",
+        openapi: "3.0.0",
         info: {
             title: "Nordvitalips API",
             version: "1.0.0",
@@ -8,18 +8,35 @@ export const options = {
         },
         servers:[
             {
+                url: "http://localhost:3600/api/v1",
+                description: "Development server Back End"
+            },
+            {
                 url: "http://localhost:5173",
-                description: "Development server"
+                description: "Development server Front End"
             },
             {
                 url: "https://test.nordvitalips.com",
-                description: "Test server"
+                description: "Test server Front End"
+            },
+            {
+                url: "api.nordvitalips.com",
+                description: "Production server Back End"
             },
             {
                 url: "https://app.nordvitalips.com",
                 description: "Production server"
             }
-        ]
+        ],
+        components: {
+            securitySchemes: {
+                bearerAuth: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT'
+                }
+            }
+        }
     },
     apis: ["./src/routes/*.ts"]
 }
