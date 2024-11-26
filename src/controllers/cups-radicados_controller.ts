94c756621e91eeabd89f2656d4175e8fa6cebbc0
Gabriel Duarte <duartearias2002@gmail.com>
2024-11-26 14:34:57 -0500

    quitando console log por error

--- src/controllers/cups-radicados_controller.ts
+++ src/controllers/cups-radicados_controller.ts
@@ -52,10 +52,7 @@ export async function createCupsRadicados(
 ) {
   try {
     // se recibe un array de objetos con los cups a radicar y el id de la radicación
-    console.log(req.body);
     const cupsRadicados: CupsRadicado[] = JSON.parse(req.body.items);
-    console.log(typeof cupsRadicados);
-    console.log(cupsRadicados)
     const { idRadicado } = req.body;
 
     // se crea un array para almacenar los cups creados
