00c7964a407164a439142e0f61ca91202bddff53
Gabriel Duarte <duartearias2002@gmail.com>
2024-11-29 15:46:54 -0500

    Ajuste permisos rol 6 (coordinador)

--- src/routes/radicacion_routes.ts
+++ src/routes/radicacion_routes.ts
@@ -333,7 +333,7 @@ router.get("/radicacion-month", authenticate, authorizeRoles(['1', '10', '3', '1
  *       404:
  *         description: No se encontraron radicaciones para ese documento
  */
-router.post('/radicado-doc-patient',authenticate, authorizeRoles(['1', '10', '3', '15', '6']), buscarRadicadoPorDocumento); 
+router.post('/radicado-doc-patient',authenticate, authorizeRoles(['1', '10','6', '3', '15', '6']), buscarRadicadoPorDocumento); 
 
 /**
  * @swagger
