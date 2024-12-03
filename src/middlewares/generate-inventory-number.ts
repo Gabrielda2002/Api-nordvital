f91183126e1963f61cc1e1f5ef1aa0cb67d45ec2
Gabriel Duarte <duartearias2002@gmail.com>
2024-12-03 10:32:44 -0500

    middleware generador numero inventario
    Se complementa el middleware para automatizar la generacion del numero de inventario de los equipos

--- src/middlewares/generate-inventory-number.ts
+++ src/middlewares/generate-inventory-number.ts
@@ -1,4 +1,6 @@
 import { NextFunction, Request, Response } from "express";
+import { LugarRadicacion } from "../entities/lugar-radicacion";
+import { Usuarios } from "../entities/usuarios";
 
 enum Deviceinitials{
     'LAPTOP' = 'LT',
@@ -22,8 +24,26 @@ enum Deviceinitials{
 export const generateInventoryNumber = async (req: Request, res: Response, next: NextFunction) => {
     try {
         
-        const { typeEquipment, sedeId, serial, name} = req.body; // * typeEquipment, el tipo de equipo, name, para el nombre de dispositivos red y perifericos
+        const { typeEquipment, sedeId, serial, name, managerId} = req.body; // * typeEquipment, el tipo de equipo, name, para el nombre de dispositivos red y perifericos
 
+        // buscar el numero de la sede y el codigo de la sede
+        const sedes = await LugarRadicacion.createQueryBuilder('lugar_radicacion')
+        .leftJoinAndSelect('lugar_radicacion.municipioRelation', 'municipio')
+        .andWhere('lugar_radicacion.id = :id', {id: sedeId})
+        .getOne();
+
+        const numberSede = sedes?.numeroSede;
+        const codeMunicipio = sedes?.municipioRelation?.municipioCode;
+
+        // buscar el responsable del dispositivo y sacar el area
+        const manager = await Usuarios.createQueryBuilder('usuarios')
+        .select('usuarios.area')
+        .andWhere('usuarios.id = :id', {id: managerId})
+        .getOne();
+        // se toma las 3 ultimas letras del area del usuario responsable
+        const area = manager?.area?.slice(0, 3);
+
+        // se inicializa el tipo de dispositivo y su inicial
         var diviceType : string;
         var diviceInitial: string;
         if (typeEquipment) {
@@ -37,7 +57,7 @@ export const generateInventoryNumber = async (req: Request, res: Response, next:
 
         const lastFourSerial = serial.slice(-4);
 
-        const inventoryNumber = `NV-${54001}-${diviceInitial}-${lastFourSerial}`;
+        const inventoryNumber = `NV-${codeMunicipio}-0${numberSede}-${area}-${diviceInitial}-${lastFourSerial}`;
 
         req.body.inventoryNumber = inventoryNumber;
 
