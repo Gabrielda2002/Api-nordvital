be846f9088a08cfa3ad5ee5d554ab04d34dd9349
Gabriel Duarte <duartearias2002@gmail.com>
2024-12-05 16:25:08 -0500

    Cambio migraciones prevencion errores
    Se elimina columna fecha de la tabla usuario por error y se agrega dirección a insert en sedes

--- src/migrations/1733176106124-alter-table-sedes.ts
+++ src/migrations/1733176106124-alter-table-sedes.ts
@@ -1,61 +1,71 @@
-import { MigrationInterface, QueryRunner, TableColumn, TableForeignKey } from "typeorm";
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableForeignKey,
+} from "typeorm";
 
 export class AlterTableSedes1733176106124 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    // Add new columns to usuario table
+    // await queryRunner.addColumns("usuario", [
+    //     new TableColumn({
+    //         name: "area",
+    //         type: "varchar",
+    //         length: "100",
+    //         isNullable: true
+    //     }),
+    //     new TableColumn({
+    //         name: "cargo",
+    //         type: "varchar",
+    //         length: "200",
+    //         isNullable: true
+    //     }),
+    //     new TableColumn({
+    //         name: "sede_id",
+    //         type: "int",
+    //         isNullable: true
+    //     }),
+    //     new TableColumn({
+    //         name: "celular",
+    //         type: "bigint",
+    //         isNullable: true
+    //     })
+    // ]);
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
+    //eliminar campo fecha de la tabla usuario
+    await queryRunner.dropColumn("usuario", "fecha");
 
-        // Add new columns to usuario table
-        await queryRunner.addColumns("usuario", [
-            new TableColumn({
-                name: "area",
-                type: "varchar",
-                length: "100",
-                isNullable: true
-            }),
-            new TableColumn({
-                name: "cargo",
-                type: "varchar",
-                length: "200",
-                isNullable: true
-            }),
-            new TableColumn({
-                name: "sede_id",
-                type: "int",
-                isNullable: true
-            }),
-            new TableColumn({
-                name: "celular",
-                type: "bigint",
-                isNullable: true
-            })
-        ]);
+    // Create foreign key for sede_id in usuario table
+    await queryRunner.createForeignKey(
+      "usuario",
+      new TableForeignKey({
+        columnNames: ["sede_id"],
+        referencedColumnNames: ["IdLugar"],
+        referencedTableName: "sedes",
+        onDelete: "CASCADE",
+      })
+    );
+  }
 
-        // Create foreign key for sede_id in usuario table
-        await queryRunner.createForeignKey("usuario", new TableForeignKey({
-            columnNames: ["sede_id"],
-            referencedColumnNames: ["IdLugar"],
-            referencedTableName: "sedes",
-            onDelete: "CASCADE"
-        }));
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    // Drop foreign key for sede_id in usuario table
+    const table = await queryRunner.getTable("usuario");
+    const foreignKey = table?.foreignKeys.find(
+      (fk) => fk.columnNames.indexOf("sede_id") !== -1
+    );
+
+    if (foreignKey) {
+      await queryRunner.dropForeignKey("usuario", foreignKey);
     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        // Drop foreign key for sede_id in usuario table
-        const table = await queryRunner.getTable("usuario");
-        const foreignKey = table?.foreignKeys.find(fk => fk.columnNames.indexOf("sede_id") !== -1);
+    // Drop new columns from usuario table
+    await queryRunner.dropColumn("usuario", "celular");
+    await queryRunner.dropColumn("usuario", "sede_id");
+    await queryRunner.dropColumn("usuario", "cargo");
+    await queryRunner.dropColumn("usuario", "area");
 
-        if (foreignKey) {
-            await queryRunner.dropForeignKey("usuario", foreignKey);
-        }
-
-
-        // Drop new columns from usuario table
-        await queryRunner.dropColumn("usuario", "celular");
-        await queryRunner.dropColumn("usuario", "sede_id");
-        await queryRunner.dropColumn("usuario", "cargo");
-        await queryRunner.dropColumn("usuario", "area");
-
-        // Drop numero_sede column from municipios table
-        await queryRunner.dropColumn("municipios", "numero_sede");
-    }
+    // Drop numero_sede column from municipios table
+    await queryRunner.dropColumn("municipios", "numero_sede");
+  }
 }
--- src/migrations/1733326521580-insert-table-sedes.ts
+++ src/migrations/1733326521580-insert-table-sedes.ts
@@ -3,11 +3,11 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 export class insertTableSedes1733326521580 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            INSERT INTO sedes (NombreLugar, Estado, departamento, ciudad, numero_sede)
+            INSERT INTO sedes (NombreLugar, Estado, direccion, departamento, ciudad, numero_sede)
             VALUES 
-            ('La Mesa', 1, 2, 4, 1),
-            ('Cajica', 1, 2, 6, 4),
-            ('Ubate', 1, 2, 5, 3)
+            ('La Mesa', 1,'Sin registrar' ,2, 4, 1),
+            ('Cajica', 1, 'Sin registrar' ,2, 6, 4),
+            ('Ubate', 1, 'Sin registrar' ,2, 5, 3)
         `);
 
             await queryRunner.query(`
