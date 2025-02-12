8171744535479bb6f5c98c55bc741563d32af01a
Gabriel Duarte <duartearias2002@gmail.com>
2025-02-12 08:40:13 -0500

    Change, permisos rutas consulta servicios y pacientes CS

--- src/routes/pacientes-coosalud_routes.ts
+++ src/routes/pacientes-coosalud_routes.ts
@@ -16,6 +16,6 @@ router.put('/pacientes-coosalud/:id', authenticate, authorizeRoles([ '1']), vali
 
 router.delete('/pacientes-coosalud/:id', authenticate, authorizeRoles([ '1']),validarId ,deletePatientCoosalud)
 
-router.post('/paciente-identificacion', authenticate, authorizeRoles([ '1']), getPatientByIdentificationCoosalud)
+router.post('/paciente-identificacion', authenticate, authorizeRoles(['1','2','3','4','5','6','10','11','12','13','14','15','16']), getPatientByIdentificationCoosalud)
 
 export default router
