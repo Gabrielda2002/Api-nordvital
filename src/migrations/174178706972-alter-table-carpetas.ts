e41e986758319c99947b00d1766fd7a524f3e369
Gabriel Duarte <duartearias2002@gmail.com>
2025-03-18 14:36:27 -0500

    fix: migracion alter table carpetas

--- src/migrations/174178706972-alter-table-carpetas.ts
+++ src/migrations/174178706972-alter-table-carpetas.ts
@@ -2,8 +2,9 @@ import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class AlterTableCarpetasAddSeccion1741787069720 implements MigrationInterface {
     
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE carpetas ADD COLUMN seccion TEXT DEFAULT 'sgc'`);
+        public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE carpetas ADD COLUMN seccion TEXT`);
+        await queryRunner.query(`UPDATE carpetas SET seccion = 'sgc'`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
