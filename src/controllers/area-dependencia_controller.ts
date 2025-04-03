09a1cf062475d843f819873e374b04da0199ffd7
Gabriel Duarte <duartearias2002@gmail.com>
2025-04-03 16:58:14 -0500

    Feat: corregir consulta para obtener todas las áreas de dependencia

--- src/controllers/area-dependencia_controller.ts
+++ src/controllers/area-dependencia_controller.ts
@@ -4,7 +4,7 @@ import { AreaDependencia } from "../entities/area-dependencia";
 export async function getAllAreaDependency(req: Request, res: Response, next: NextFunction){
     try {
         
-        const areaDependencia = await AreaDependencia.find({});
+        const areaDependencia = await AreaDependencia.find();
         
         if (areaDependencia.length === 0) {
             return res.status(404).json({ message: "No se encontraron registros." });
