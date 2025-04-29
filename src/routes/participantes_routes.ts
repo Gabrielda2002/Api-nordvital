09c35ada52badfd1755710892905e4511cca61fc
Gabriel Duarte <duartearias2002@gmail.com>
2025-04-29 08:50:21 -0500

    feat: agregar endpoints para la gestión de participantes y sus esquemas en Swagger

--- src/routes/participantes_routes.ts
+++ src/routes/participantes_routes.ts
@@ -5,8 +5,59 @@ import { createParticipant, getAllParticipants } from "../controllers/participan
 
 const router = Router();
 
+/**
+ * @swagger
+ * /participants:
+ *   get:
+ *     summary: Obtener todos los participantes registrados
+ *     tags: [Participantes]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Lista de participantes.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Participante'
+ *       500:
+ *         description: Error interno del servidor.
+ */
 router.get('/participants', authenticate, authorizeRoles(['1']), getAllParticipants);
 
+/**
+ * @swagger
+ * /participants:
+ *   post:
+ *     summary: Crear un nuevo participante
+ *     tags: [Participantes]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/ParticipanteInput'
+ *     responses:
+ *       201:
+ *         description: Participante creado exitosamente.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                 participant:
+ *                   $ref: '#/components/schemas/Participante'
+ *       400:
+ *         description: Error de validación o participante ya existe.
+ *       500:
+ *         description: Error interno del servidor.
+ */
 router.post('/participants', authenticate, authorizeRoles(['1']), createParticipant);
 
 export default router;
