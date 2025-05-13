d02678e9eaa417d96da4439a347ff54f337f235f
Gabriel Duarte <duartearias2002@gmail.com>
2025-05-13 16:00:37 -0500

    feat: agregar nuevas rutas para estadísticas de televisores por sede, edad y vencimiento de garantía

--- src/routes/televisor_routes.ts
+++ src/routes/televisor_routes.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { authenticate } from "../middlewares/auth";
 import { authorizeRoles } from "../middlewares/authorize-roles";
 import { validarId } from "../middlewares/validar-id";
-import { createTelevisor, getTelevisorBySedeId, updateTelevisor } from "../controllers/televisor_controller";
+import { createTelevisor, getTelevisorBySedeId, getTvAgeByHeadquarter, getTvHeadquartersDistribution, getTvWarrantyStatistics, updateTelevisor } from "../controllers/televisor_controller";
 
 const router = Router();
 
@@ -11,5 +11,11 @@ router.get('/inventario/televisores-sede/:id', authenticate, authorizeRoles(['1'
 router.post('/televisores', authenticate, authorizeRoles(['1']), createTelevisor);
 
 router.put('/televisores/:id', authenticate, authorizeRoles(['1']), validarId, updateTelevisor);
+
+router.get('/tv/statics/headquarters', authenticate, authorizeRoles(['1']), getTvHeadquartersDistribution);
+
+router.get('/tv/statics/age', authenticate, authorizeRoles(['1']), getTvAgeByHeadquarter);
+
+router.get('/tv/statics/warrantyExpiration', authenticate, authorizeRoles(['1']), getTvWarrantyStatistics);
 
 export default router;
