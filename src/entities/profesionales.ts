f9dedf76e298a0469025a3f7d45003dc541f7675
Gabriel Duarte <duartearias2002@gmail.com>
2025-06-26 10:20:10 -0500

    feat: agregar controlador y rutas para gestionar profesionales

--- src/entities/profesionales.ts
+++ src/entities/profesionales.ts
@@ -1,11 +1,14 @@
 import { BaseEntity, Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToMany, UpdateDateColumn } from "typeorm";
 import { Radicacion } from "./radicacion";
+import { IsNotEmpty, IsString } from "class-validator";
 @Entity("profesionales")
 export class Profesionales extends BaseEntity { 
     @Column({ name: "id", primary: true, type: "int", generated: true })
     id: number;
 
     @Column({ name: "nombre", type: "varchar", length: 255 })
+    @IsNotEmpty({ message: "El nombre del profesional es requerido" })
+    @IsString({ message: "El nombre del profesional debe ser una cadena de texto" })
     name: string;
 
     @CreateDateColumn({ name: "created_at", type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
