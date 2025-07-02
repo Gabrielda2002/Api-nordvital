b4912f9d94e33a45201e21eba4c92ad75c1b0b02
Gabriel Duarte <duartearias2002@gmail.com>
2025-07-02 15:39:34 -0500

    refactor: mejorar la estructura y legibilidad de las entidades AccesoriosEquipos y Componentes

--- src/entities/accesorios-equipos.ts
+++ src/entities/accesorios-equipos.ts
@@ -1,65 +1,90 @@
-import { IsBoolean, IsNotEmpty, IsString, Length } from "class-validator"
-import { BaseEntity, Column, CreateDateColumn, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm"
-import { Equipos } from "./equipos"
+import { IsBoolean, IsNotEmpty, IsString, Length } from "class-validator";
+import {
+  BaseEntity,
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { Equipos } from "./equipos";
 
-@Entity({name: "accesorios_equipos"})
+@Entity({ name: "accesorios_equipos" })
 export class AccesoriosEquipos extends BaseEntity {
+  @PrimaryGeneratedColumn({ name: "id" })
+  id: number;
 
-    @PrimaryGeneratedColumn({name: "id"})
-    id: number
+  @Column({ name: "equipo_id" })
+  equipmentId: number;
 
-    @Column({name: "equipo_id"})
-    equipmentId: number
+  @Column({ name: "nombre" })
+  @IsString()
+  @IsNotEmpty({ message: "El nombre del accesorio es requerido" })
+  @Length(3, 255, {
+    message:
+      "El nombre del accesorio debe tener entre $constraint1 y $constraint2 caracteres",
+  })
+  name: string;
 
-    @Column({name: "nombre"})
-    @IsString()
-    @IsNotEmpty({message: "El nombre del accesorio es requerido"})
-    @Length(3, 255, {message: "El nombre del accesorio debe tener entre $constraint1 y $constraint2 caracteres"})
-    name: string
+  @Column({ name: "marca" })
+  @IsString()
+  @IsNotEmpty({ message: "La marca del accesorio es requerida" })
+  @Length(2, 255, {
+    message:
+      "La marca del accesorio debe tener entre $constraint1 y $constraint2 caracteres",
+  })
+  brand: string;
 
-    @Column({name: "marca"})
-    @IsString()
-    @IsNotEmpty({message: "La marca del accesorio es requerida"})
-    @Length(2, 255, {message: "La marca del accesorio debe tener entre $constraint1 y $constraint2 caracteres"})
-    brand: string
+  @Column({ name: "modelo" })
+  @IsString()
+  @IsNotEmpty({ message: "El modelo del accesorio es requerido" })
+  model: string;
 
-    @Column({name: "modelo"})
-    @IsString()
-    @IsNotEmpty({message: "El modelo del accesorio es requerido"})
-    model: string
+  @Column({ name: "serial" })
+  @IsString()
+  @IsNotEmpty({ message: "El serial del accesorio es requerido" })
+  @Length(3, 255, {
+    message:
+      "El serial del accesorio debe tener entre $constraint1 y $constraint2 caracteres",
+  })
+  serial: string;
 
-    @Column({name: "serial"})
-    @IsString()
-    @IsNotEmpty({message: "El serial del accesorio es requerido"})
-    @Length(3, 255, {message: "El serial del accesorio debe tener entre $constraint1 y $constraint2 caracteres"})
-    serial: string
+  @Column({ name: "otros_datos" })
+  @IsString()
+  @IsNotEmpty({ message: "Otros datos del accesorio son requeridos" })
+  @Length(3, 255, {
+    message:
+      "Los otros datos del accesorio deben tener entre $constraint1 y $constraint2 caracteres",
+  })
+  otherData: string;
 
-    @Column({name: "otros_datos"})
-    @IsString()
-    @IsNotEmpty({message: "Otros datos del accesorio son requeridos"})
-    @Length(3, 255, {message: "Los otros datos del accesorio deben tener entre $constraint1 y $constraint2 caracteres"})
-    otherData: string
+  @Column({ name: "estado" })
+  @IsString()
+  @IsNotEmpty({ message: "El estado del accesorio es requerido" })
+  @Length(3, 255, {
+    message:
+      "El estado del accesorio debe tener entre $constraint1 y $constraint2 caracteres",
+  })
+  status: string;
 
-    @Column({name: "estado"})
-    @IsString()
-    @IsNotEmpty({message: "El estado del accesorio es requerido"})
-    @Length(3, 255, {message: "El estado del accesorio debe tener entre $constraint1 y $constraint2 caracteres"})
-    status: string
+  @Column({ name: "numero_inventario" })
+  @IsString()
+  @IsNotEmpty({ message: "El número de inventario del accesorio es requerido" })
+  @Length(3, 255, {
+    message:
+      "El número de inventario del accesorio debe tener entre $constraint1 y $constraint2 caracteres",
+  })
+  inventoryNumber: string;
 
-    @Column({name: "numero_inventario"})
-    @IsString()
-    @IsNotEmpty({message: "El número de inventario del accesorio es requerido"})
-    @Length(3, 255, {message: "El número de inventario del accesorio debe tener entre $constraint1 y $constraint2 caracteres"})
-    inventoryNumber: string
+  @CreateDateColumn({ name: "created_at" })
+  createdAt: Date;
 
-    @CreateDateColumn({name: "created_at"})
-    createdAt: Date
+  @UpdateDateColumn({ name: "updated_at" })
+  updatedAt: Date;
 
-    @UpdateDateColumn({name: "updated_at"})
-    updatedAt: Date
-
-    @ManyToOne(() => Equipos, equipment => equipment.accessoriesRelation)
-    @JoinColumn({name: "equipo_id"})
-    equipmentRelation: Equipos
-
+  @ManyToOne(() => Equipos, (equipment) => equipment.accessoriesRelation)
+  @JoinColumn({ name: "equipo_id" })
+  equipmentRelation: Equipos;
 }
